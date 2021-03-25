dbec758c759278c1bc2632f597e67bb38d31f614
sungho.kim <sungho2.kim@sk.com>
2021-03-25 10:05:03 +0900

    Update the schema of instances table in 'ingest_module'

--- ingest_modules/instances.scala
+++ ingest_modules/instances.scala
@@ -8,7 +8,7 @@ import IngestModule._
 
 
 
-  class Instances( var instance_id: Long, var project_id: Long, var dataset_id: Long, var  file_id: Long, var cropped_image: String, var track_id: Long, var binding_id: Long, var class_name: String, var region_type: String, var region: String, var create_date: String, var update_date: String , var  feature_value: Array[Float], var feature_value_3d: String, var model_id: String, var extracting_method: String, var gender: String, var age: String, var expression: String, var action: String, var posture: String, var color: String, var cropped_image_path: String , var file_id_key: Long, var model_id_is_not_null: Boolean, var tags: String )
+  class Instances( var instance_id: Long, var project_id: Long, var dataset_id: Long, var  file_id: Long, var cropped_image: String, var track_id: Long, var binding_id: Long, var class_name: String, var region_type: String, var region: String, var create_date: String, var update_date: String , var  feature_value: Array[Float], var feature_value_3d: String, var model_id: String, var extracting_method: String, var gender: String, var age: String, var expression: String, var action: String, var posture: String, var color: String, var cropped_image_path: String , var file_id_key: Long, var model_id_is_not_null: Boolean, var tags: String, var motion_score: Double, var confidence_score: Double )
 
 
   case class Data (var instances: Array[Instances])
